a90d422dc23881e9a92109a06f66a94455000801
Siddhant-balwadkar <siddhantvbalwadkar@gmail.com>
2026-01-10 18:09:43 +0530

    Add files via upload

---
+++ Assignment 13/program13_2.c
@@ -0,0 +1,57 @@
+
+// print all even numbers up to N
+
+#include<stdio.h>
+
+////////////////////////////////////////////////////////////////////
+//
+//  Function Name : print_even_numbers
+//  Description :   print all even number from 1 to N
+//  Input :         Integer
+//  Output :        Integer
+//  Author :        Siddhant Vikas Balwadkar
+//  Date :          30/12/2025
+//
+////////////////////////////////////////////////////////////////////
+
+void print_even_numbers(int limit)
+{
+    int iCnt = 0;
+
+    //Updator
+    if(limit < 0)
+    {
+        limit = -limit;
+    }
+
+    for(iCnt = 1; iCnt <= limit; iCnt++)
+    {
+        if((iCnt % 2) == 0)
+        {
+            printf("%d\t",iCnt);
+        }
+    }
+}
+
+int main()
+{
+    int limit;
+
+    printf("Enter number : ");
+    scanf("%d", &limit);
+    print_even_numbers(limit);
+
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////////
+
+//  Input :      5
+//  Output :     2 4
+
+//  Input :      8
+//  Output :     2 4 6 8
+
+// Time Complexity : O(N)
+
+///////////////////////////////////////////////////////////////////
